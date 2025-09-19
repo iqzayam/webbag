34a307e948334460d94c4571f143469766bd82fa
iqzayam <akbarfaid6@gmail.com>
2025-09-19 22:35:35 +0700

    Delete public/users.js

--- public/users.js
+++
@@ -1,7 +0,0 @@
-const Database = require("better-sqlite3");
-const db = new Database("tokens.db");
-
-module.exports = (req, res) => {
-  const rows = db.prepare("SELECT * FROM users").all();
-  return res.json(rows);
-};
